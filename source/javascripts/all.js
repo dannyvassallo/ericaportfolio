59614a70d682eb24db56ed8dd2e9cd3e3b31f616
dannyvassallo <danielvassallo87@gmail.com>
2015-06-08 04:12:02 +0000

    added slider for header section

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,7 @@
+//= require modernizr.custom.79639
 //= require jquery
 //= require bootstrap
+//= require jquery.ba-cond.min
+//= require jquery.slitslider
+//= require initheader
 //= require_tree .
